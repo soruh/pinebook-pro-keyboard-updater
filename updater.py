f2379b87e9b3dd302b02647d28a216933d6a0e3e
Akira Kyle <ak@akirakyle.com>
2020-04-26 20:28:44 -0600

    more updates to updater.py and added another few functions to fw_ansi.namelist

--- updater.py
+++ updater.py
@@ -14,9 +14,6 @@ USER_MODE_CONFIG = USB_MODE_CONFIG(vid=0x258a, pid=0x001e, iface=1)
 # may also need to try product IDs 0x001f and 0x000d
 BOOT_MODE_CONFIG = USB_MODE_CONFIG(vid=0x0603, pid=0x1020, iface=0)
  
-class DeviceNotFoundError(Exception):
-    pass
-
 def read_hexdata(hex_path):
     ih = IntelHex(hex_path)
     ih.padding = 0x00
@@ -60,47 +57,66 @@ def write_block(devh, data, offset, length):
   devh.controlWrite(CTRL_OUT, usb1.REQUEST_SET_CONFIGURATION, 0x0306, 0,
                     transfer, timeout=2000)
 
-def write_bulk(devh, data):
-    write_block_start(devh, len(length))
+def write_bulk(devh, data, attempts=5):
+    sucess = False
+    for _ in range(attempts):
+        try:
+            write_block_start(devh, len(length))
 
-    # HACK: overwrite first byte (as in original sources)
-    first_byte = data[0]
-    data[0] = 0
+            # HACK: overwrite first byte (as in original sources)
+            first_byte = data[0]
+            data[0] = 0
 
-    for offset in range(0, len(data), BLOCK_SIZE):
-        write_block(devh, data, offset, BLOCK_SIZE)
-        sleep(0.01)
+            for offset in range(0, len(data), BLOCK_SIZE):
+                write_block(devh, data, offset, BLOCK_SIZE)
+                sleep(0.01)
 
-    data[0] = first_byte
+            data[0] = first_byte
 
-    # retry write of first block
-    write_block_start(devh, length);
-    write_block(devh, data, 0, BLOCK_SIZE);
+            # retry write of first block
+            write_block_start(devh, length);
+            write_block(devh, data, 0, BLOCK_SIZE);
+            sucess = True
+            break
+        except usb1.USBError as e:
+            print(e, e.value)
+    if not sucess:
+        raise Exception("too many tries, aborting...")
 
-def read_bulk(devh, length):
-    #read_block_start(devh, length);
-    print(">>> USB: read_block_start (length={})".format(length))
+def read_bulk(devh, length, attempts=5):
+    sucess = False
+    for _ in range(attempts):
+        try:
+            #read_block_start(devh, length);
+            print(">>> USB: read_block_start (length={})".format(length))
 
-    transfer = bytearray([
-        0x05, # report id
-        0x52,
-        0x00,
-        0x00,
-        length & 0xFF,
-        (length >> 8) & 0xFF])
+            transfer = bytearray([
+                0x05, # report id
+                0x52,
+                0x00,
+                0x00,
+                length & 0xFF,
+                (length >> 8) & 0xFF])
 
-    devh.controlWrite(CTRL_OUT, usb1.REQUEST_SET_CONFIGURATION, 0x0305, 0,
-                      transfer, timeout=100)
+            devh.controlWrite(CTRL_OUT, usb1.REQUEST_SET_CONFIGURATION,
+                              0x0305, 0, transfer, timeout=100)
 
-    data = bytearray()
+            data = bytearray()
 
-    block_size = 2048
-    for offset in range(0, len(data), block_size):
-        #read_block(devh, data, offset, block_size);
-        print(">>> USB: read_block (offset={}, length={})".format(offset, length))
-        data += devh.controlRead(CTRL_IN, usb1.REQUEST_CLEAR_FEATURE, 0x0306, 0,
-                                 length, timeout=2000)
-        sleep(0.01)
+            block_size = 2048
+            for offset in range(0, len(data), block_size):
+                #read_block(devh, data, offset, block_size);
+                print(">>> USB: read_block (offset={}, length={})".format(
+                    offset, length))
+                data += devh.controlRead(CTRL_IN, usb1.REQUEST_CLEAR_FEATURE,
+                                         0x0306, 0, length, timeout=2000)
+                sleep(0.01)
+            sucess = True
+            break
+        except usb1.USBError as e:
+            print(e, e.value)
+    if not sucess:
+        raise Exception("too many tries, aborting...")
 
 def open_usb(context, vid, pid, iface, attempts=1):
     for _ in range(attempts):
@@ -112,7 +128,7 @@ def open_usb(context, vid, pid, iface, attempts=1):
         if devh is not None: break
 
     if devh is None:
-            raise DeviceNotFoundError
+        raise Exception(">>> Device not found")
 
     if devh.kernelDriverActive(iface):
         print(">>> Kernel Driver Active\n");
@@ -138,6 +154,12 @@ def switch_to_boot_mode(context):
                     BOOT_MODE_CONFIG.iface, attempts=20)
     return devh
 
+def erase_flash(devh):
+    print("[*] Erasing flash")
+    reportData = bytearray([0x05, 0x45, 0x45, 0x45, 0x45, 0x45])
+    devh.controlWrite(CTRL_OUT, usb1.REQUEST_SET_CONFIGURATION, 0x0305, 0,
+                      reportData, timeout=100)
+
 def reset_device(devh):
     data = bytearray([0x05, 0x55, 0x55, 0x55, 0x55, 0x55])
     devh.controlWrite(CTRL_OUT, usb1.REQUEST_SET_CONFIGURATION, 0x0305, 0,
@@ -153,47 +175,31 @@ def flash_kb(context, hex_path):
     hex_data = read_hexdata(hex_path)
     devh = switch_to_boot_mode(context)
 
-    reportData = bytearray([0x05, 0x45, 0x45, 0x45, 0x45, 0x45])
+    try:
+        erase_flash(devh)
+        sleep(2)
 
-    print("[*] Erasing flash")
-    devh.controlWrite(CTRL_OUT, usb1.REQUEST_SET_CONFIGURATION, 0x0305, 0,
-                      reportData, timeout=100)
+        print("[*] Writing firmware...")
+        write_bulk(devh, hex_data)
 
-    sleep(2)
+        print("[*] Reading back firmware...")
+        read_hex = read_bulk(devh, len(hex_data))
 
-    print("[*] Writing firmware...")
-    for _ in range(5):
-        try:
-            write_bulk(devh, hex_data)
-            break
-        except Exception as e:
-            print(e)
-    
-    print("[*] Reading back firmware...")
-    for _ in range(5):
-        try:
-            read_hex = read_bulk(devh, len(hex_data))
-            break
-        except Exception as e:
-            print(e)
-    if read_hex is None:
-        raise Exception("too many tries")
+        print("[*] Comparing firmwares...")
+        if hex_data[:0x37fb] != read_hex[:0x37fb]:
+            for i in range(len(hex_data)):
+                if hex_data[i] != read_hex[i]:
+                    print(">>> 0x{:04x}] {:02x} != {:02x}".format(
+                        i, hex_data[i], read_hex[i]))
+            raise Exception("FATAL ERROR FW does differ")
 
+        print("[*] Reseting device?")
+        reset_device(devh)
 
-    print("[*] Comparing firmwares...")
-    if hex_data[:0x37fb] != read_hex[0x37fb]:
-        print("FATAL ERROR FW does differ")
-        for i in range(len(hex_data)):
-            if hex_data[i] != read_hex[i]:
-                print(">>> 0x{:04x}] {:02x} != {:02x}".format(
-                    i, hex_data[i], read_hex[i]))
-        raise Exception("FATAL ERROR FW does differ")
+        print("[*] Finished succesfully!")
 
-    print("[*] Reseting device?")
-    reset_device(devh)
-
-    print("[*] Finished succesfully!")
-    close_usb(devh, BOOT_MODE_CONFIG.iface)
+    finally:
+        close_usb(devh, BOOT_MODE_CONFIG.iface)
 
 def usage():
     print("usage: {} path/to/keyboard/firmware.hex".format(sys.argv[0]))
